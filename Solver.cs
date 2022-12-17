381d61526e2e4ea05ef8c082f55ebec761eb74ca
Kristoffer Smed <mail@kristoffersmed.dk>
2022-12-17 22:22:45 +0100

    Clean up.

--- Solver.cs
+++ Solver.cs
@@ -1,37 +1,16 @@
-using System.Collections.Concurrent;
 using System.Collections.Immutable;
 
 namespace JuleSudoku;
 
 internal static class Solver
 {
-    private static readonly ConcurrentDictionary<Field, int> Updates = new();
-
     private static Board? _solution;
-    
-    private static (ParallelOptions, CancellationTokenSource) SetupParallelism()
-    {
-        var cts = new CancellationTokenSource();
-        var po = new ParallelOptions
-        {
-            CancellationToken = cts.Token,
-            MaxDegreeOfParallelism = Environment.ProcessorCount
-        };
-        Console.WriteLine($"Running in parallel: {Environment.ProcessorCount}");
 
-        return (po, cts);
-    }
-    
     public static Board? Solve(Board board)
     {
         var predeterminedValues = board.Locked.Select(board.GetField);
         var availableValues = Enumerable.Range(1, 25).ToList();
         availableValues.RemoveAll(x => predeterminedValues.Contains(x));
-
-        // Initialize Updates
-        for (var row = 0; row < Board.Size; row++)
-        for (var column = 0; column < Board.Size; column++)
-            Updates.TryAdd(new Field(row, column), 0);
 
         using var tokenSource = new CancellationTokenSource();
         var po = new ParallelOptions
@@ -56,8 +35,6 @@ internal static class Solver
     private static void TrySolve(Board board, Field field, ImmutableArray<int> availableValues,
         CancellationTokenSource tokenSource, ParallelOptions parallelOptions)
     {
-        var subTasks = new List<Task<Board?>>(availableValues.Length);
-
         Parallel.For(0, availableValues.Length, parallelOptions, i =>
         {
             // Run backwards as we want to test biggest numbers first.
@@ -65,8 +42,6 @@ internal static class Solver
             var newBoard = board.SetField(value, field);
             if (!Validator.ValidateField(newBoard, field, availableValues.Take(5).ToList()))
                 return;
-
-            Updates.AddOrUpdate(field, 0, (_, x) => x + 1); // Always updates - '0' will never be added.
 
             var nextField = FindNextField(newBoard, field);
             var remainingValues = availableValues.RemoveAt(i);
